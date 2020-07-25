65f7604f405b4a04ddf14c910885fc66ca90ec53
JLob Lap <joselobatof@gmail.com>
2020-07-25 02:51:57 -0500

    Cambios para preguntar por primos muy altos

--- nuevo_primos.py
+++ nuevo_primos.py
@@ -1,14 +1,14 @@
 # Descartamos números de Carmichael
 def comprobacion(numero):
-    for i in range(1, 100):
+    for i in range(1, 1000000):
         fermat = (2 ** (i - 1)) % i
         if fermat == 1:
             division = int(numero/i)
             resto = numero % i
-            if resto == 0:
-                return False
             if division <= i:
                 return True
+            if resto == 0:
+                return False
 
 
 # Por teorema de Fermat para números primos
@@ -26,6 +26,10 @@ def run():
         print("Es primo")
     else:
         print("No es primo")
+
+    # encuentra hasta el 9497 en un for del 1 al 9,498
+    # preguntando si es primo hasta ahora con el 990,000,023
+
 
 
 if __name__ == "__main__":
