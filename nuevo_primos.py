632753b27f5dd79fd138a71ed6727a1097ce2384
JLob Lap <joselobatof@gmail.com>
2020-07-26 14:26:36 -0500

    Comentarios adicionales

--- nuevo_primos.py
+++ nuevo_primos.py
@@ -1,4 +1,4 @@
-# Descartamos números de Carmichael
+# Descartamos números de Carmichael (numeros compuestos por otros números primos)
 def comprobacion(numero):
     for i in range(1, 1000000):
         fermat = (2 ** (i - 1)) % i
@@ -29,7 +29,6 @@ def run():
 
     # encuentra hasta el 9497 en un for del 1 al 9,498
     # preguntando si es primo hasta ahora con el 990,000,023
-
 
 
 if __name__ == "__main__":
